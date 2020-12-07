2a5c7c2a50267b62b3a419f2558ba30c87d38f4b
arejea <47055032+arejea@users.noreply.github.com>
2020-12-07 14:33:41 -0500

    Update ChannelVideoActorTest.java
    test included

--- play-java-forms-example/test/actors/ChannelVideoActorTest.java
+++ play-java-forms-example/test/actors/ChannelVideoActorTest.java
@@ -1,1 +1,65 @@
+package controllers;
 
+import actors.ChannelApi;
+import actors.ChannelVideoActor;
+import actors.ChannelVideoProtocol;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.testkit.javadsl.TestKit;
+import models.Search;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+
+/**
+ * This test was written to test the ChannelVideoActorClass.
+ * @author adeyinka
+ */
+
+public class ChannelVideoActorTest {
+
+    static ActorSystem system;
+    static ChannelVideoProtocol.SayHello testService;
+
+
+    @BeforeClass
+    public static void setup() {
+        system = ActorSystem.create();
+        testService = new ChannelVideoProtocol.SayHello("UC88MUln48JnCz4VeuPmiNPQ");
+    }
+
+    @AfterClass
+    public static void teardown() {
+        TestKit.shutdownActorSystem(system);
+        system = null;
+    }
+
+
+    @Test
+    public void testIt() {
+        /*
+         * Wrap the whole test procedure within a testkit constructor
+         * if you want to receive actor replies or use Within(), etc.
+         */
+        new TestKit(system) {
+            {
+
+
+                final Props props = Props.create(ChannelVideoActor.class);
+                final ActorRef subject = system.actorOf(props);
+
+                final TestKit probe = new TestKit(system);
+
+                subject.tell(testService, probe.getRef());
+                probe.expectMsgClass(ArrayList.class);
+
+            }
+        };
+    }
+
+
+}
