66697f599b470c4f04da175e4b500eb9e2a9a08d
link2hitesh <ankur.96469@gmail.com>
2020-12-10 21:14:12 -0500

    Java Docs

--- play-java-forms-example/app/models/SearchDatas.java
+++ play-java-forms-example/app/models/SearchDatas.java
@@ -4,13 +4,20 @@ import models.Data;
 
 import java.util.ArrayList;
 import java.util.List;
-
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+/**
+ * This class creates an object that contains the search terms and its associated list of videos.
+ *  @author Adeyinka
+ */
 public class SearchDatas{
+
+    /**
+     * Setter for search terms
+     * @param searchTerm
+     */
 
     public void setSearchTerm(String searchTerm) {
         this.searchTerm = searchTerm;
@@ -22,6 +29,9 @@ public class SearchDatas{
 
     public List<Data> result;
 
+    /**
+     * constructor
+     */
     public SearchDatas(){
         searchTerm = "";
     result = new ArrayList<Data>();
--- play-java-forms-example/app/models/SearchValues.java
+++ play-java-forms-example/app/models/SearchValues.java
@@ -8,30 +8,48 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+
+/**
+ * This class creates an object that stores the details of each video in its attributes.
+ * @author Adeyinka
+ */
+
+
 public class SearchValues {
     /**
      * items represents a collection of searches
      */
     private List<models.SearchDatas> items;
 
-
+    /**
+     * constructor
+     */
     public SearchValues() {
 
         this.items = new ArrayList<models.SearchDatas>();
     }
-
+    /**
+     * Add results in the list
+     */
 
     public void addSearchResult(models.SearchDatas searchResult){
 
         items.add(searchResult);
 
     }
+    /**
+     *  return list of results
+     * @return  items
+     */
 
     public List<models.SearchDatas> list(){
 
         return items;
     }
 
+    /**
+     * Add all results to the list
+     */
 
     public void addAll(SearchValues newItems){
         for(int i=0; i< newItems.list().size(); i++){
@@ -40,6 +58,9 @@ public class SearchValues {
 
     }
 
+    /**
+     * remove all results to the list
+     */
     public void removeAll(SearchValues newItems){
         for(int i=0; i< newItems.list().size(); i++){
             items.remove(newItems.list().get(i));
