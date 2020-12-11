87e85d378b0f59372720d50a8e56e6cb1d139193
HITESH AGARWAL <hitesh0981@gmail.com>
2020-12-10 20:48:53 -0500

    Update YoutubeServiceApi.java
    Add JavaDocs

--- play-java-forms-example/app/service/YoutubeServiceApi.java
+++ play-java-forms-example/app/service/YoutubeServiceApi.java
@@ -4,6 +4,11 @@ import java.util.concurrent.CompletionStage;
 import java.util.*;
 import models.*;
 
+/**
+ * @AdeyinkaAreje
+ * This is the interface YoutubeServiceApi  which is implemented in the Service class.
+ */
+
 public interface YoutubeServiceApi {
 
     public CompletionStage<List<String>> search(String word);
